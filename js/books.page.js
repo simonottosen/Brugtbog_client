5c05146f72ee5d14db81d89c68829ea5c85e0a75
Simon Ottosen <simonottosen@Simons-Mac-Pro.local>
2016-11-08 22:02:02 +0100

    1st version

--- js/books.page.js
+++ js/books.page.js
@@ -3,46 +3,40 @@
 $(document).ready(function(){
     var $booksISBN = $("#booksISBN");
 
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        xhrFields: { withCredentials: true },
+        url: "https://localhost:8000/login",
+        data: JSON.stringify({
+            "username" : "test",
+            "password" : "1234"
+        }),
 
-$.ajax({
-    url: "https://momentify.eu.ngrok.io/api/books",
-    method: "GET",
-    headers: {
-        filter: JSON.stringify({"include":"authors"})
-    },
-    dataTypes: "json",
-    contentType: "application/json",
-    success: function(books, status, xhr){
-
-        function printAuthors(authors) {
-            return authors.map(function (author){
-                return author.firstName +" "+ author.lastName;
-                }).join(", ");
-        }
-
-        books.forEach(function(book){
-            $booksISBN.append(
-                "<tr>" +
-                "<td>"+ book.price +"</td>" +
-                "<td>" + book.title + "</td>" +
-                "<td>" + book.subtitle +"</td>" +
-                "<td>" + printAuthors(book.authors) + "</td>" +
-                "</tr>"
-            );
-        });
-
-
-
-
-    },
-    error: function (xhr, status, error){
-        console.log(xhr,status, error)
-    },
-
+        success: function( data ) {//alert(JSON.stringify(data));
+         },
+        error: function( data ) { alert(JSON.stringify(data)); }
     });
 
 
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        xhrFields: { withCredentials: true },
+        url: "https://localhost:8000/getbooks",
 
-
-})
-
+        success: function( data ) {
+          data.forEach(function(data){
+            $booksISBN.append(
+                "<tr>" +
+                "<td>" + data.isbn +"</td>" +
+                "<td>" + data.title + "</td>" +
+                "<td>" + data.edition +"</td>" +
+                "<td>" + data.author + "</td>" +
+                "</tr>"
+            );
+          })
+        },
+        error: function( data ) { alert(JSON.stringify(data)); }
+    });
+});
