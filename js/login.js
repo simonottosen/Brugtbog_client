e8c3793a7353f3fb835c22a836d7e4979be45b45
simonottosen <simnik123@gmail.com>
2016-11-27 16:25:33 +0100

    Improved structure

---
+++ js/login.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function(){
+    var $usercontrol = $("#usercontrol");
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        xhrFields: { withCredentials: true },
+        url: "https://localhost:8000/login",
+        data: JSON.stringify({
+            "username" : "test",
+            "password" : "1234"
+        }),
+
+        success: function( data ) {//alert(JSON.stringify(data));
+         },
+        error: function( data ) { alert(JSON.stringify(data)); }
+    });
+
+
+});
