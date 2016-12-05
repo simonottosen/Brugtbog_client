4435ae9621a2a0634acb10186d3736d0db0eedf3
simonottosen <simnik123@gmail.com>
2016-12-05 09:37:26 +0100

    ChangeAd minor changes

--- js/user/change_my_ad.page.js
+++ js/user/change_my_ad.page.js
@@ -8,19 +8,13 @@
           url: "https://localhost:8000/updatead",
           data: JSON.stringify({
             "adId": $("#adId").val(),
-            "isbn": $("#isbn").val(),
-            "bookTitle": $("#bookTitle").val(),
-            "bookAuthor": $("#bookAuthor").val()
-            "bookEdition": $("#bookEdition").val(),
-            "rating": $("#rating").val(),
-            "price": $("#price").val(),
-            "userUsername": $("#userUsername").val()
-            "transfer": $("#transfer").val(),
-            "mobilepay": $("#mobilepay").val(),
-            "cash": $("#cash").val()
+            "isbn" : parseInt($("#isbn").val()),
+            "rating" : parseInt($("#rating").val()),
+            "comment" : $("#comment").val(),
+            "price" : parseInt($("#price").val())
 
       }),
-      success: function( data ) {//alert(JSON.stringify(data));
+      success: function( data ) {alert(JSON.stringify(data));
        },
       error: function( data ) { alert(JSON.stringify(data)); }
       });
